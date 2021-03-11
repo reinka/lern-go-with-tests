daff3e3f745b180f91b9f487dc1bf181e5237a0b
apoehlmann <andrei.poehlmann90@gmail.com>
2021-03-11 10:30:53 +0100

    sync, mutex, WaitGroup, go vet; Don't use embedding because it's convenient

---
+++ src/sync/counter.go
@@ -0,0 +1,22 @@
+package sync
+
+import "sync"
+
+type Counter struct {
+	mu    sync.Mutex
+	count int
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.count++
+}
+
+func (c *Counter) Value() int {
+	return c.count
+}
