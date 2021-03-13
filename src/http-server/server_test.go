1884042566ea986b14e614f0ce2f4ad6d492ed67
apoehlmann <andrei.poehlmann90@gmail.com>
2021-03-13 15:49:09 +0100

    Initial commit.

---
+++ src/http-server/server_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGETPlayers(t *testing.T) {
+
+	t.Run("get Pepper's score", func(t *testing.T) {
+		request := newGetScoreRequest("Pepper")
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+		assertResponseBody(t, response.Body.String(), "20")
+	})
+
+	t.Run("get Floyd's score", func(t *testing.T) {
+		request := newGetScoreRequest("Floyd")
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		assertResponseBody(t, response.Body.String(), "10")
+	})
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	return httptest.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+}
+
+func assertResponseBody(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
