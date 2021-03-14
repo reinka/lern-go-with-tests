8d405f02a927cfca28b1916f3d6d484caad0895c
apoehlmann <andrei.poehlmann90@gmail.com>
2021-03-14 12:46:40 +0100

    Add integration test.

--- src/http-server/main.go
+++ src/http-server/main.go
@@ -6,17 +6,9 @@ import (
 )
 
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
 }
-
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) (int, bool) {
-	return 123, true
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {}
