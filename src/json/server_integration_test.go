fde6f305968b5b39f10e53666bcb56dc39c805d3
apoehlmann <andrei.poehlmann90@gmail.com>
2021-03-19 17:43:54 +0100

    Add integration test for the JSON endpoint.

--- src/json/server_integration_test.go
+++ src/json/server_integration_test.go
@@ -15,10 +15,22 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 
-	response := httptest.NewRecorder()
-	server.ServeHTTP(response, newGetScoreRequest(player))
+	t.Run("get score", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newGetScoreRequest(player))
 
-	assertStatus(t, response.Code, http.StatusOK)
+		assertStatus(t, response.Code, http.StatusOK)
+		assertResponseBody(t, response.Body.String(), "3")
+	})
 
-	assertResponseBody(t, response.Body.String(), "3")
+	t.Run("get league", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		request := newLeagueRequest()
+
+		server.ServeHTTP(response, request)
+		got := getLeagueFromResponse(t, response.Body)
+
+		assertLeague(t, got, []Player{{player, 3}})
+		assertContentType(t, response, jsonContentType)
+	})
 }
